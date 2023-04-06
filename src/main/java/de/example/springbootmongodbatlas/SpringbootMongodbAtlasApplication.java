506ef48e925706c17305be41ad45b29fde1912bb
Florian Walliser <f.walliser@quality-miners.de>
2023-04-06 09:27:02 +0200

    @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class}) deleted

--- src/main/java/de/example/springbootmongodbatlas/SpringbootMongodbAtlasApplication.java
+++ src/main/java/de/example/springbootmongodbatlas/SpringbootMongodbAtlasApplication.java
@@ -1,12 +1,9 @@
 package de.example.springbootmongodbatlas;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SpringbootMongodbAtlasApplication {
 
 	public static void main(String[] args) {
